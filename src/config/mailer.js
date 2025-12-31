ab1bb6957d2ad7360e8e56ca6e3e066e512446f7
Joseph Oluwapelumi Elijah <joluwapelumi027@gmail.com>
2025-12-31 18:14:14 +0100

    fix mailer issue

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,21 +1,20 @@
-import dotenv from "dotenv";
-dotenv.config(); // load env before using it
-
-
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 transporter.verify((error, success) => {
   if (error) console.log("Mailer verification failed:", error);
-  else{ 
-    console.log("Mailer is ready ✅");
-  }
+  else console.log("Mailer is ready ✅");
 });
 
 export default transporter;
